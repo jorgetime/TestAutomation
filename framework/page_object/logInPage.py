95e5424344a11bd35b694cf19b2f938d4d7bbad1
J <jorgedontstop@gmai.com>
2018-08-19 18:28:01 +0100

    clean up

--- framework/page_object/logInPage.py
+++ framework/page_object/logInPage.py
@@ -1,6 +1,4 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
+
 from framework.page_object.base_pages.baseHudlPage import BaseWebPage
 from selenium.webdriver.common.by import By
 
